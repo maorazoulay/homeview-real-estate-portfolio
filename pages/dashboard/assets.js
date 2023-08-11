891c77c3848c216fb330f585c08437a33610e68d
maorazoulay <maorazoulay@gmail.com>
2023-08-11 17:35:57 -0400

    Added assets page

---
+++ pages/dashboard/assets.js
@@ -0,0 +1,62 @@
+import { React, useState, useEffect } from "react"
+import Image from "next/image"
+import Link from "next/link"
+import { getAllAssetsForUser } from "@/api"
+import DashboardLayout from "@/components/layouts/DashboardLayout"
+
+export default function Assets() {
+  const [assets, setAssets] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    async function loadAssets(userId){
+      setLoading(true)
+      try{
+        const assets = await getAllAssetsForUser(userId)
+        setAssets(assets)
+      } catch(error){
+        setError(error)
+      } finally {
+        setLoading(false)
+      }
+    }
+    // Hardcode userId for now
+    loadAssets(1)
+  }, [])
+
+  // get assets from miragejs server
+  // map and display them here
+  const assetElements = assets.map(asset => {
+    return (
+      <Link href={`/dashboard/assets/${asset.id}`} 
+      className="cursor-pointer hvr-grow asset-item gap-2" key={asset.id}>
+        <Image
+          src={asset.images[0]}
+          alt="property image"
+          // fill
+          width={400}
+          height={400}
+          priority
+        />
+        <div className="asset-info">
+          <p>{asset.title}</p>
+          <p>{asset.address}</p>
+          <p>{asset.propertyType}</p>
+        </div>
+      </Link>
+    )
+  })
+
+  return (
+    <div className="assets">
+      {assetElements}
+    </div>
+  )
+}
+
+Assets.getLayout = function getLayout(page){
+    return (
+        <DashboardLayout>{page}</DashboardLayout>
+    )
+}
