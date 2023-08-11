3048257d4d54bf9d1f8c53bf7b5a41cf06ac2e11
maorazoulay <maorazoulay@gmail.com>
2023-08-11 17:35:47 -0400

    Added the overview page

---
+++ components/ValueChart.js
@@ -0,0 +1,54 @@
+import { Card, Title, AreaChart } from "@tremor/react";
+
+const chartdata = [
+  {
+    date: "Jan 22",
+    "Assets Value": 200000,
+    "Equity Value": 50000,
+  },
+  {
+    date: "Feb 22",
+    "Assets Value": 220000,
+    "Equity Value": 53000,
+  },
+  {
+    date: "Mar 22",
+    "Assets Value": 250000,
+    "Equity Value": 57000,
+  },
+  {
+    date: "Apr 22",
+    "Assets Value": 255000,
+    "Equity Value": 63000,
+  },
+  {
+    date: "May 22",
+    "Assets Value": 272000,
+    "Equity Value": 79000,
+  },
+  {
+    date: "Jun 22",
+    "Assets Value": 279000,
+    "Equity Value": 86000,
+  },
+];
+
+const dataFormatter = (number) => {
+  return "$ " + Intl.NumberFormat("us").format(number).toString();
+};
+
+export default function ValueChart() {
+    return (
+      <Card>
+        <Title>Portfolio value over time (USD)</Title>
+        <AreaChart
+          className="h-72 mt-4"
+          data={chartdata}
+          index="date"
+          categories={["Assets Value", "Equity Value"]}
+          colors={["indigo", "cyan"]}
+          valueFormatter={dataFormatter}
+        />
+      </Card>
+    );
+} 
---
+++ pages/dashboard/overview.js
@@ -0,0 +1,101 @@
+import DashboardLayout from "@/components/layouts/DashboardLayout";
+
+
+import {
+    Grid, Col, Card, Text, Metric,
+    Flex, BadgeDelta, Title, DonutChart, Legend
+  } from "@tremor/react";
+  import ValueChart from "@/components/ValueChart";
+  
+  export default function Overview() {
+    const assets = [
+      {
+        name: "Single-Family",
+        units: 5,
+        value: 500000
+      },
+      {
+        name: "Condo",
+        units: 2,
+        value: 300000
+      },
+      {
+        name: "Multi-Family",
+        units: 1,
+        value: 250000
+      },
+      {
+        name: "lot",
+        units: 3,
+        value: 300000
+      },
+    ];
+    const colors = ["slate", "violet", "indigo", "rose", "cyan", "amber"]
+    const legendCategories = assets.map(asset => asset.name)
+  
+    const breakdownFormatter = (number) => `${number.toString()} Assets`;
+    const valueFormatter = (number) => `$ ${Intl.NumberFormat("us").format(number).toString()}`;
+  
+    return (
+      <>
+        <Grid numItems={1} numItemsSm={2} numItemsLg={3} className="gap-2 p-6">
+          <Col>
+            <Card className="w-full">
+              <Title>Assets Type Breakdown</Title>
+              <DonutChart
+                className="mt-6"
+                data={assets}
+                category="units"
+                index="name"
+                valueFormatter={breakdownFormatter}
+                colors={colors}
+                variant='donut' />
+              <Legend
+                className="mt-3 flex justify-center"
+                categories={legendCategories}
+                colors={colors} />
+            </Card>
+          </Col>
+          <Col>
+            <Card className="h-full">
+              <Flex justifyContent="between" alignItems="center">
+                <Text className="total font-bold">Portfolio value</Text>
+                <BadgeDelta deltaType="moderateIncrease" isIncreasePositive={true} size="xs">
+                  +12.3%
+                </BadgeDelta>
+              </Flex>
+              <Metric className="value text-center text-6xl mt-32">
+                $ 1,350,000
+              </Metric>
+            </Card>
+          </Col>
+          <Col>
+            <Card className="w-full">
+              <Title>Assets Value Breakdown</Title>
+              <DonutChart
+                className="mt-6"
+                data={assets}
+                category="value"
+                index="name"
+                valueFormatter={valueFormatter}
+                colors={colors}
+                variant='pie' />
+              <Legend
+                className="mt-3 flex justify-center"
+                categories={legendCategories}
+                colors={colors} />
+            </Card>
+          </Col>
+        </Grid>
+        <div className="px-6">
+          <ValueChart />
+        </div>
+      </>
+    );
+  } 
+  
+Overview.getLayout = function getLayout(page){
+    return (
+        <DashboardLayout>{page}</DashboardLayout>
+    )
+}
