95b1df54f53c2872a18e8c476ed91dbf875f623a
maorazoulay <maorazoulay@gmail.com>
2023-08-11 17:35:26 -0400

    itegrated layouts across all app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout || ((page) => page)
+ 
+  return getLayout(<Component {...pageProps} />)
 }
