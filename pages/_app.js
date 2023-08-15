a8aae34306e6752e2363bd744540f9adb64ce5ac
maorazoulay <maorazoulay@gmail.com>
2023-08-15 16:44:31 -0400

    Added new goals to roadmap

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,10 @@ export default function MyApp({ Component, pageProps }) {
   return getLayout(<Component {...pageProps} />)
 }
 
-// TODO: MongoDB integration with mocked data
 // TODO: Authenticatation
-// TODO: build out API
-// TODO: Add option to add assets
+// TODO: Use Authenticatation to get userId
+// TODO: build out API - WIP
+
+// Tech Debt:
+// Fix assets layout when there is less than 3 items in one row
+// Fix Image dimensions in assets page - contain the image in a div and use fill
