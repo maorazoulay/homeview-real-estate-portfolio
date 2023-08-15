4a22926d8682df593407b7a2043e62dfc1407f5e
maorazoulay <maorazoulay@gmail.com>
2023-08-15 15:32:27 -0400

    Accounting - WIP

--- pages/dashboard/accounting.js
+++ pages/dashboard/accounting.js
@@ -1,25 +1,20 @@
 import { useEffect, useState } from "react"
 import DashboardLayout from "@/components/layouts/DashboardLayout"
-import { createAsset, getAllAssetsForUser } from "@/apiBridge"
+import { insertAsset, getAllAssetsForUser } from "@/apiBridge"
 
 export default function Accounting() {
     const [response, setResponse] = useState({})
 
     useEffect(() => {
-        getAllAssetsForUser(1)
-        // createAsset({
-        //     userId: 1,
-        //     title: "Mondrian",
-        //     address: "1100 West Ave",
-        //     propertyType: "Condo",
-        //     images: ["1100_west.jpg"],
-        //     purchasePrice: 600000,
-        //     purchaseDate: Date.now(),
-        //     marketValue: 720000
-        // })
-            .then(res => {
-                console.log(res)
+
+        getAllAssetsForUser(1).then(assets => {
+            assets.forEach(asset => {
+                insertAsset(asset)
+                    .then(res => {
+                        console.log(res)
+                    })
             })
+        })
     }, [])
 
     // console.log(response);
