8e6bdc8e4c24a7f7bfb9941ae007753787f4fbf8
maorazoulay <maorazoulay@gmail.com>
2023-08-14 23:33:29 -0400

    Addedd assets endpoint

--- pages/api/assets.js
+++ pages/api/assets.js
@@ -1,10 +1,31 @@
-export default function handler(req, res) {
-    if(req.method === 'GET'){
-        // Get the assets for current user
-    } else if(req.method === 'POST'){
-        // Add a new asset for current user
-    } else if(req.method === 'UPDATE'){
-        // Edit the asset for current user
+import dbConnect from "@/db/dbConnect"
+import Asset from "@/db/Asset"
+
+export default async function handler(req, res) {
+    const { method } = req
+
+    await dbConnect()
+
+    switch (method) {
+        case 'GET':
+            try {
+                const assets = await Asset.find({})
+                res.status(200).json({ success: true, data: assets })
+            } catch (error) {
+                res.status(400).json({ success: false })
+            }
+            break
+        case 'POST':
+            try {
+                const asset = await Asset.create(req.body)
+                console.log("Asset: ", asset);
+                res.status(201).json({ success: true, data: asset })
+            } catch (error) {
+                res.status(400).json({ success: false })
+            }
+            break
+        default:
+            res.status(400).json({ success: false })
+            break
     }
-    res.status(200).json({ name: 'John Doe' })
-  }
+}
