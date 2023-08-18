c8520d121f1cb6e182ab586ae144a1df9dee068c
maorazoulay <maorazoulay@gmail.com>
2023-08-17 23:21:43 -0400

    some useful instructions for later

--- pages/dashboard/assets/[id].js
+++ pages/dashboard/assets/[id].js
@@ -6,6 +6,9 @@ export default function Page() {
   return <p>ID: {router.query.id}</p>
 }
 
+// Add getStaticPaths function here...
+//  see https://nextjs.org/docs/pages/building-your-application/data-fetching/get-static-paths
+
 Page.getLayout = function getLayout(page){
   return (
       <DashboardLayout>{page}</DashboardLayout>
