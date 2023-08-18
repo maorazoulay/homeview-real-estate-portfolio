116e0d8ce162dc4647c8e75785f4f5b49eddaaaa
maorazoulay <maorazoulay@gmail.com>
2023-08-18 14:27:26 -0400

    Expanded DB Ops

--- db/dbOperations.js
+++ db/dbOperations.js
@@ -1,11 +1,31 @@
 import Asset from "./Asset"
 
-export async function readUserAssets(userId){
-    const assets = await Asset.find({userId: userId}).lean()
+export async function readUserAssets(userId) {
+    const assets = await Asset.find({ userId: userId })
     return JSON.parse(JSON.stringify(assets))
 }
 
-export async function insertNewAsset(data){
-    const asset = await Asset.insert(JSON.stringify(data)).lean()
+export async function insertNewAsset(data) {
+    const asset = await Asset.insert(JSON.stringify(data))
+    return JSON.parse(JSON.stringify(asset))
+}
+
+export async function findAssetById(assetId) {
+    const asset = await Asset.findById(assetId)
+    return JSON.parse(JSON.stringify(asset))
+}
+
+export async function updateAsset(assetId, data) {
+    const asset = await Asset.findByIdAndUpdate(
+        assetId,
+        JSON.stringify(data), {
+        new: true,
+        runValidators: true,
+    })
+    return JSON.parse(JSON.stringify(asset))
+}
+
+export async function deleteAsset(assetId) {
+    const asset = await Asset.deleteOne({ _id: assetId })
     return JSON.parse(JSON.stringify(asset))
 }
