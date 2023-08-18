5bf5325c3282d2cb5c2a49df324fc7fdb9e09ccf
maorazoulay <maorazoulay@gmail.com>
2023-08-17 23:22:46 -0400

    more tech debt

--- pages/_app.js
+++ pages/_app.js
@@ -12,5 +12,7 @@ export default function MyApp({ Component, pageProps }) {
 // TODO: build out API - WIP
 
 // Tech Debt:
+// configure cache control headers for SSR, see: https://nextjs.org/docs/pages/building-your-application/deploying/production-checklist#caching
 // Fix assets layout when there is less than 3 items in one row
 // Fix Image dimensions in assets page - contain the image in a div and use fill
+// Add the env variable NEXTAUTH_URL='cannonical URL' in production?
