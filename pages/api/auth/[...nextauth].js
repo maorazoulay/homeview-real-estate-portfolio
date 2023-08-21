32a315481561d0a91607a02318e23df04c0fdb9b
maorazoulay <maorazoulay@gmail.com>
2023-08-21 18:40:47 -0400

    Fixed callback config in nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,20 +1,38 @@
+import { getUserId } from "@/db/dbOperations";
 import NextAuth from "next-auth"
 // import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
     // GithubProvider({
     //   clientId: process.env.GITHUB_ID,
     //   clientSecret: process.env.GITHUB_SECRET,
     // }),
-    GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      })
     // ...add more providers here
   ],
+  callbacks: {
+    async jwt({ token, account, profile }) {
+      // Persist the OAuth access_token and or the user id to the token right after signin
+      if (account) {
+        token.account = account
+        token.profile = profile
+      }
+      return token
+    },
+    async session({session, token}) {
+      // Send properties to the client, like a user id from the db.
+      session.token = token
+      const dbUserId = await getUserId(session.user.email)
+      session.user.id = dbUserId
+      
+      return session
+    }
+  }
 }
 
 export default NextAuth(authOptions)
