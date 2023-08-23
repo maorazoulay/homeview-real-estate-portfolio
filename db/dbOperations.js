96a900b43f7e6eabcb9c4f70c5183c9cc4f62ddb
maorazoulay <maorazoulay@gmail.com>
2023-08-23 16:58:40 -0400

    Fixed getUserId and also insertNewUser

--- db/dbOperations.js
+++ db/dbOperations.js
@@ -14,16 +14,19 @@ export async function readUserAssets(userId) {
 }
 
 export async function insertNewAsset(data) {
+    await dbConnect()
     const asset = await Asset.insert(JSON.stringify(data))
     return JSON.parse(JSON.stringify(asset))
 }
 
 export async function findAssetById(assetId) {
+    await dbConnect()
     const asset = await Asset.findById(assetId)
     return JSON.parse(JSON.stringify(asset))
 }
 
 export async function updateAsset(assetId, data) {
+    await dbConnect()
     const asset = await Asset.findByIdAndUpdate(
         assetId,
         JSON.stringify(data), {
@@ -34,17 +37,24 @@ export async function updateAsset(assetId, data) {
 }
 
 export async function deleteAsset(assetId) {
+    await dbConnect()
     const asset = await Asset.deleteOne({ _id: assetId })
     return JSON.parse(JSON.stringify(asset))
 }
 
 export async function insertNewUser(data) {
-    const user = await User.insert(JSON.stringify(data))
-    return JSON.parse(JSON.stringify(user))
+    await dbConnect()
+    const user = await User.create({
+        email: data.email,
+        name: data.name,
+        image: data.image,
+        signUpDate: Date.now()
+    })
+    return JSON.parse(JSON.stringify(await user.save()))
 }
 
 export async function getUserId(userEmail) {
     await dbConnect()
     const user = await User.findOne({ email: userEmail })
-    return JSON.parse(JSON.stringify(user))._id
+    return JSON.parse(JSON.stringify(user))?._id || ""
 }
