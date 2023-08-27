5ba62cbb4aa2586a1f13a1ca6cf5d0ba8b979e77
maorazoulay <maorazoulay@gmail.com>
2023-08-26 21:56:21 -0400

    Now using a new and inproved layout

--- components/DashboardLayout.js
+++ components/DashboardLayout.js
@@ -1,22 +1,10 @@
 import React from 'react'
-import Link from 'next/link'
+import Sidebar from './Sidebar'
 
 function DashboardLayout({ children }){
-    const options = ["Overview", "Assets", "Accounting", "Settings"]
-    const optionElements = options.map(option => {
-      return (<Link href={`/dashboard/${option.toLowerCase()}`} key={option} 
-                  className={`hvr-underline-from-right p-4 text-3xl text-white font-sans flex 
-                    hover:cursor-pointer
-                   font-bold sidebar-option`}>
-                  {option}
-              </Link>)
-    })
-
     return (
-        <div className="dashboard-container">
-            <div className="sidebar">
-                {optionElements}
-            </div>
+        <div className="flex">
+            <Sidebar />
             <main>
                 {children}
             </main>
