a2e86801cf58f3228f5398f773ec05597c6edab2
maorazoulay <maorazoulay@gmail.com>
2023-08-29 22:36:24 -0400

    Fixed attribute name

--- components/Topbar.js
+++ components/Topbar.js
@@ -6,9 +6,9 @@ export default function Topbar() {
     return (
         <nav class="fixed z-50 w-full h-12 bg-white shadow dark:bg-gray-800">
             <div class="container flex items-center p-4 justify-end text-gray-600 capitalize dark:text-gray-300">
-                <a href="#" class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">filter 1</a>
-                <a href="#" class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">filter 2</a>
-                <a href="#" class="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">filter 3</a>
+                <a href="#" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">filter 1</a>
+                <a href="#" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">filter 2</a>
+                <a href="#" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">filter 3</a>
                 <Link href={'/'} className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">
                     <BsHouseAddFill />
                 </Link>
