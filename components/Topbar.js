64a50fd8973fa4be80b609e377530c71c4b35ee5
maorazoulay <maorazoulay@gmail.com>
2023-08-31 20:01:26 -0400

    Fixed class warning

--- components/Topbar.js
+++ components/Topbar.js
@@ -4,8 +4,8 @@ import Modal from "@/components/Modal";
 
 export default function Topbar() {
     return (
-        <nav class="fixed z-50 w-full h-12 bg-white shadow dark:bg-gray-800">
-            <div class="container flex items-center p-4 justify-end text-gray-600 capitalize dark:text-gray-300">
+        <nav className="fixed z-50 w-full h-12 bg-white shadow dark:bg-gray-800">
+            <div className="container flex items-center p-4 justify-end text-gray-600 capitalize dark:text-gray-300">
                 <a href="#" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">filter 1</a>
                 <a href="#" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">filter 2</a>
                 <a href="#" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">filter 3</a>
