82f5da4f650a83a00a315c375f758644d37ffd0c
maorazoulay <maorazoulay@gmail.com>
2023-09-02 23:10:10 -0400

    Added attributes for image input

--- components/AssetForm.js
+++ components/AssetForm.js
@@ -1,5 +1,5 @@
 
-export default function Form() {
+export default function AssetForm() {
     async function addNewAsset(event) {
         // const hostedImagesPath = await hostImages(formData.images)
         event.preventDefault()
@@ -56,7 +56,7 @@ export default function Form() {
                                         <label htmlFor="marketValue" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Market Value</label>
                                     </div>
                                     <div className="relative">
-                                        <input type="file" id="images" autoComplete="off" name="images" className="peer placeholder-transparent h-10 w-full border-b-2 border-indigo-500 text-gray-900 focus:outline-none focus:borer-rose-600" />
+                                        <input type="file" multiple accept="image/*" id="images" autoComplete="off" name="images" className="peer placeholder-transparent h-10 w-full border-b-2 border-indigo-500 text-gray-900 focus:outline-none focus:borer-rose-600" />
                                         <label htmlFor="images" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Images</label>
                                     </div>
                                     <div className="relative">
