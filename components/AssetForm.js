34b5728cdd2c187328ef7aabfa1aff23456416c1
maorazoulay <maorazoulay@gmail.com>
2023-09-02 22:44:11 -0400

    Fixed Asset Form layout and submisison logic

--- components/AssetForm.js
+++ components/AssetForm.js
@@ -1,56 +1,73 @@
-import { useState } from "react";
 
-import {
-    Card,
-    Input,
-    Button,
-    Typography,
-} from "@material-tailwind/react";
+export default function Form() {
+    async function addNewAsset(event) {
+        // const hostedImagesPath = await hostImages(formData.images)
+        event.preventDefault()
+        const formData = new FormData(event.target)
+        let data = {}
+        for (const [key, value] of formData) {
+            data[key] = value
+        }
 
-export default function AssetForm() {
-    const [formData, setFormData] = useState({})
-
-    function updateForm(event) {
-        const name = event.target.name;
-        const value = event.target.value;
-        setFormData(values => ({ ...values, [name]: value }))
-    }
-
-    async function addNewAsset() {
         const response = await fetch('/api/assets', {
             method: 'POST',
-            body: JSON.stringify(formData),
+            body: JSON.stringify(data)
         })
 
         console.log('New asset inserted', await response.json().data);
     }
 
     return (
-        <Card color="transparent" shadow={true}>
-            {/* <Typography variant="h4" color="indigo">
-                Add New Asset
-            </Typography> */}
-            <Typography color="gray" className="mt-1 font-bold">
-                Enter the details of the new asset.
-            </Typography>
-            <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-                <Typography color="indigo">
-                    <div className="mb-4 flex flex-col gap-6">
-                        <Input type="text" name="title" value={formData.title || ""} onChange={updateForm} size="lg" placeholder="Property Title" />
-                        <Input type="text" name="address" value={formData.address || ""} onChange={updateForm} size="lg" placeholder="Address" />
-                        <Input type="text" name="propertyType" value={formData.propertyType || ""} onChange={updateForm} size="lg" placeholder="Property Type" />
-                        <Input type="number" name="purchasePrice" value={formData.purchasePrice} onChange={updateForm} size="lg" placeholder="Purchase Price" />
-                        <Input type="date" name="purchaseDate" value={formData.purchaseDate || Date.now} onChange={updateForm} size="lg" placeholder="Purchase Date" />
-                        <Input type="number" name="marketValue" value={formData.marketValue} onChange={updateForm} size="lg" placeholder="Market Value" />
-                        <Input type="file" name="images" value={formData.images || []} onChange={updateForm} multiple="multiple" size="lg" placeholder="images" />
+        <div className="relative min-h-screen bg-transparent py-6 flex flex-col justify-center sm:py-12">
+            <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+                <div
+                    className="absolute inset-0 bg-gradient-to-r from-indigo-400 to-indigo-700 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+                </div>
+                <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+                    <div className="max-w-md mx-auto">
+                        <div>
+                            <h1 className="text-2xl font-semibold">Add New Asset</h1>
+                        </div>
+                        <form onSubmit={addNewAsset}>
+                            <div className="divide-y divide-gray-200">
+                                <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                                    <div className="relative">
+                                        <input type="text" id="title" autoComplete="off" name="title" className="peer placeholder-transparent h-10 w-full border-b-2 border-indigo-500 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Title" />
+                                        <label htmlFor="title" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Property Title</label>
+                                    </div>
+                                    <div className="relative">
+                                        <input type="text" id="address" autoComplete="off" name="address" className="peer placeholder-transparent h-10 w-full border-b-2 border-indigo-500 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Address" />
+                                        <label htmlFor="address" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Address</label>
+                                    </div>
+                                    <div className="relative">
+                                        <input type="text" id="propertyType" autoComplete="off" name="propertyType" className="peer placeholder-transparent h-10 w-full border-b-2 border-indigo-500 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Property Type" />
+                                        <label htmlFor="propertyType" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Property Type</label>
+                                    </div>
+                                    <div className="relative">
+                                        <input type="date" id="purchaseDate" autoComplete="off" name="purchaseDate" className="peer placeholder-transparent h-10 w-full border-b-2 border-indigo-500 text-gray-900 focus:outline-none focus:borer-rose-600" />
+                                        <label htmlFor="purchaseDate" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Purchase Date</label>
+                                    </div>
+                                    <div className="relative">
+                                        <input type="number" id="purchasePrice" autoComplete="off" name="purchasePrice" className="peer placeholder-transparent h-10 w-full border-b-2 border-indigo-500 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Purchase Price" />
+                                        <label htmlFor="purchasePrice" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Purchase Price</label>
+                                    </div>
+                                    <div className="relative">
+                                        <input type="number" id="marketValue" autoComplete="off" name="marketValue" className="peer placeholder-transparent h-10 w-full border-b-2 border-indigo-500 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Market Value" />
+                                        <label htmlFor="marketValue" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Market Value</label>
+                                    </div>
+                                    <div className="relative">
+                                        <input type="file" id="images" autoComplete="off" name="images" className="peer placeholder-transparent h-10 w-full border-b-2 border-indigo-500 text-gray-900 focus:outline-none focus:borer-rose-600" />
+                                        <label htmlFor="images" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Images</label>
+                                    </div>
+                                    <div className="relative">
+                                        <button type="submit" className="bg-indigo-500 hvr-grow text-white w-full mt-2 rounded-md px-2 py-1">Submit</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-
-                </Typography>
-
-                <Button onClick={addNewAsset} className="mt-6 bg-indigo-700" fullWidth>
-                    Add Asset
-                </Button>
-            </form>
-        </Card>
-    );
+                </div>
+            </div >
+        </div >
+    )
 }
