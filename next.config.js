ce2a948843bbd0b958df052729a99f28d9a86809
maorazoulay <maorazoulay@gmail.com>
2023-09-06 13:42:48 -0400

    Added functionlaity to host user images in S3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'homeview-app.s3.us-east-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  }
 }
 
 module.exports = nextConfig
