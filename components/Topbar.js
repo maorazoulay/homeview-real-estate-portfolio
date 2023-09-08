f27eecda5a18237de9ecc4a2a0e1529b9c6da47a
maorazoulay <maorazoulay@gmail.com>
2023-09-08 16:50:05 -0400

    Attempted to fix button position - still WIP

--- components/Topbar.js
+++ components/Topbar.js
@@ -6,8 +6,7 @@ export default function Topbar() {
             <div className="container flex items-center p-4 justify-start sm:justify-center md:justify-end text-gray-600 capitalize dark:text-gray-300">
                 <a href="#" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">filter 1</a>
                 <a href="#" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">filter 2</a>
-                <a href="#" className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">filter 3</a>
-                <Modal className="border-b-2 border-transparent hover:text-gray-800 dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6" />
+                <Modal className="place-self-end" />
             </div>
         </nav>
     )
