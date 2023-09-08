f6a6240a2b606c4de8dcae78dd0f41a26037ae88
maorazoulay <maorazoulay@gmail.com>
2023-09-07 22:14:58 -0400

    Now using tailwind for assets layout styling

--- pages/dashboard/assets/index.js
+++ pages/dashboard/assets/index.js
@@ -43,7 +43,7 @@ export default function Assets({ assets }) {
     return (
         <>
             <Topbar />
-            <div className="assets">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pt-20 p-10">
                 {assetElements}
             </div>
         </>
