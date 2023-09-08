04798fe5724210ae49b5df37979ac6249c31f318
maorazoulay <maorazoulay@gmail.com>
2023-09-07 20:08:27 -0400

    Minor css change

--- components/AssetForm.js
+++ components/AssetForm.js
@@ -146,7 +146,7 @@ export default function AssetForm() {
                                                 <label htmlFor="marketValue" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm required">Market Value</label>
                                             </div>
                                             <div className="relative">
-                                                <input type="file" multiple key={imageKey} accept="image/*" id="images" autoComplete="off" name="images" onChange={handleImageChange} className="peer placeholder-transparent h-10 w-full border-b-2 border-indigo-500 text-gray-900 focus:outline-none focus:borer-rose-600" />
+                                                <input type="file" multiple key={imageKey} accept="image/*" id="images" autoComplete="off" name="images" onChange={handleImageChange} className="mt-2 peer placeholder-transparent h-10 w-full border-b-2 border-indigo-500 text-gray-900 focus:outline-none focus:borer-rose-600" />
                                                 <label htmlFor="images" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm required">Images</label>
                                             </div>
                                             <div className="relative">
