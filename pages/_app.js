bad20b80682983fd725a0b7908835c92ac5aa94f
maorazoulay <maorazoulay@gmail.com>
2023-09-10 12:59:00 -0400

    Updated goals

--- pages/_app.js
+++ pages/_app.js
@@ -12,13 +12,11 @@ export default function MyApp({ Component, pageProps: { session, ...pageProps } 
     </SessionProvider>)
 }
 
-// TODO: Fix Image dimensions in assets page - contain the image in a div and use fill
 // TODO: overview - connect numbers to assets
 // TODO: Create Asset page with update and delete functionality
 // TODO: Add more NextAuh providers
 // TODO: Style Log In page
 // TODO: Style Account tab in sidebar
-// TODO: Fix currently selected tab in sidebar
 // TODO: Fix Topbar - Add filters?
 // configure cache control headers for SSR, see: https://nextjs.org/docs/pages/building-your-application/deploying/production-checklist#caching
 // Add the env variable NEXTAUTH_URL='cannonical URL' in production? - env vars in Vercel Dashboard
