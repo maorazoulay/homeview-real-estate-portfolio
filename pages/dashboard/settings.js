5053dcea3b3e8beb79328b2976791cb7c39053fc
maorazoulay <maorazoulay@gmail.com>
2023-09-10 12:57:33 -0400

    Fixed settings endpoint

--- pages/dashboard/settings.js
+++ pages/dashboard/settings.js
@@ -1,10 +1,9 @@
 import DashboardLayout from "@/components/DashboardLayout";
-import AssetForm from "@/components/AssetForm";
 
 export default function Settings() {
     return (
-        <div className="ml-20">
-            <AssetForm />
+        <div>
+            <h1>Settings here...</h1>
         </div>
 
     )
