59e3680a4e3e05c9801479986333433a5b13cc24
maorazoulay <maorazoulay@gmail.com>
2023-09-14 12:43:48 -0400

    removed unused code

--- utils/serverUtils.js
+++ utils/serverUtils.js
@@ -2,20 +2,3 @@ export const UNAUTHORIZED = {
     status: 401,
     error: 'Unauthorized, Please authenticate'
 }
-
-const imageTypes = [
-    "image/apng",
-    "image/bmp",
-    "image/gif",
-    "image/jpeg",
-    "image/pjpeg",
-    "image/png",
-    "image/svg+xml",
-    "image/tiff",
-    "image/webp",
-    "image/x-icon",
-];
-
-function validateImageType(file) {
-    return imageTypes.includes(file.type);
-}
