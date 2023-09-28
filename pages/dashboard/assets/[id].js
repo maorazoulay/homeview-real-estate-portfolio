c3a462bc9ec722b2a950441136e63dcb5efa1dbf
maorazoulay <maorazoulay@gmail.com>
2023-09-27 23:13:39 -0400

    Implemented delete asset and loading state

--- pages/dashboard/assets/[id].js
+++ pages/dashboard/assets/[id].js
@@ -1,29 +1,52 @@
+import { useState } from "react";
 import { useRouter } from 'next/router'
 import DashboardLayout from '@/components/DashboardLayout'
 import useSWR from 'swr'
 import SimpleImageSlider from "react-simple-image-slider";
 import { priceFormatter, dateFormatter } from '@/utils/formUtils';
+import ConfirmationDialog from '@/components/ConfirmationDialog';
+import Spinner from "@/components/Spinner";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
-
-const images = [
-  { url: "/3.webp" },
-  { url: "/4.webp" },
-  { url: "/5.webp" },
-  { url: "/6.webp" },
-  { url: "/7.webp" },
-  { url: "/8.webp" },
-  { url: "/9.webp" },
-];
 export default function Page() {
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [loading, setLoading] = useState(false);
   const router = useRouter()
   const { id } = router.query
 
-  const { data: response, error } = useSWR(id ? `/api/assets/${router.query.id}` : null, id ? fetcher : null)
+  const { data: response, error } = useSWR(id ? `/api/assets/${router.query.id}` : null, fetcher)
   const asset = response?.data
 
   if (error) return <div>Failed to load</div>
-  if (!response) return <div>Loading...</div>
+  if (!response) return <Spinner />
+
+  const images = asset.images.map(image => {
+    return { url: image }
+  })
+
+  function editAsset() {
+
+  }
+
+  function deleteAsset() {
+    setLoading(true)
+    console.log('deleting', router.query.id);
+    fetch(`/api/assets/${router.query.id}`, { method: 'DELETE' })
+      .then(response => response.json())
+      .then(responseData => {
+        if (responseData.success) {
+          router.push('/dashboard/assets')
+        } else {
+          console.log('Failed to delete asset');
+        }
+      }).catch((error) => {
+        console.error(`Error while trying to delete: ${error.message}`);
+      })
+  }
+
+  function closeDeleteModal(){
+    setShowDeleteModal(false)
+  }
 
   return (
     <div className='h-full pt-2'>
@@ -36,7 +59,7 @@ export default function Page() {
             images={images}
             showBullets={true}
             showNavs={true}
-            style={{ perspective: '1px', borderRadius: '0.75rem'}}
+            style={{ perspective: '1px', borderRadius: '0.75rem' }}
           />
         </div>
         <div className='flex flex-col w-full h-full justify-between bg-indigo-50 rounded-xl p-3'>
@@ -49,22 +72,27 @@ export default function Page() {
           </ul>
           <div className='flex justify-center border-t-2 border-indigo-600 p-2 gap-8'>
             <button
-              className="hvr-buzz-out bg-indigo-700 text-white active:bg-indigo-900 font-bold text-sm px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              className="hvr-hang bg-indigo-700 text-white active:bg-indigo-900 font-bold text-sm px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
               type="button">Edit Asset
             </button>
             <button
-              className="hvr-buzz-out bg-red-600 text-white active:bg-red-800 font-bold text-sm px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-              type="button">Delete Asset
+              className="hvr-hang bg-red-600 text-white active:bg-red-800 font-bold text-sm px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              type="button"
+              onClick={() => setShowDeleteModal(true)}>Delete Asset
             </button>
+            {showDeleteModal && !loading && <ConfirmationDialog
+              message={'Are you sure you want to delete this asset?'}
+              actionButtonText={'Delete'}
+              actionFunction={deleteAsset} 
+              closeFunction={closeDeleteModal}/>
+            }
+            {loading && <Spinner />}
           </div>
         </div>
       </div>
     </div>
   )
 }
-
-// You should fetch data on the client side in here, use the SWR library:
-// https://nextjs.org/docs/pages/building-your-application/data-fetching/client-side#client-side-data-fetching-with-swr
 
 Page.getLayout = function getLayout(page) {
   return (
