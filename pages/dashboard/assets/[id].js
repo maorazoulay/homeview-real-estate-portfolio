d7b46cc0f377eba4e80488f8a99b6789e33f8a7b
maorazoulay <maorazoulay@gmail.com>
2023-09-28 22:44:01 -0400

    Numerous changes to asset page - almost complete. Now using server props, also using Form component inside a modal

--- pages/dashboard/assets/[id].js
+++ pages/dashboard/assets/[id].js
@@ -1,37 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react"
 import { useRouter } from 'next/router'
 import DashboardLayout from '@/components/DashboardLayout'
-import useSWR from 'swr'
-import SimpleImageSlider from "react-simple-image-slider";
-import { priceFormatter, dateFormatter } from '@/utils/formUtils';
-import ConfirmationDialog from '@/components/ConfirmationDialog';
-import Spinner from "@/components/Spinner";
-
-const fetcher = (...args) => fetch(...args).then((res) => res.json())
-export default function Page() {
+import SimpleImageSlider from "react-simple-image-slider"
+import { priceFormatter, dateFormatter, getClassesForSubmitButton, handleDisablingButton } from '@/utils/formUtils'
+import ConfirmationDialog from '@/components/ConfirmationDialog'
+import Spinner from "@/components/Spinner"
+import { findAssetById } from "@/db/dbOperations"
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "@/pages/api/auth/[...nextauth]"
+import EditAssetForm from "@/components/EditAssetForm"
+export default function Asset({ asset }) {
+  const [tempAsset, setTempAsset] = useState(asset)
   const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [disabledSubmit, setDisabledSubmit] = useState(false)
   const router = useRouter()
-  const { id } = router.query
+  const assetId = asset._id
 
-  const { data: response, error } = useSWR(id ? `/api/assets/${router.query.id}` : null, fetcher)
-  const asset = response?.data
+  console.log('tempAsset', tempAsset);
 
-  if (error) return <div>Failed to load</div>
-  if (!response) return <Spinner />
+  useEffect(() => {
+    // enable submit button when all values are provided
+    handleDisablingButton(tempAsset, disabledSubmit, setDisabledSubmit)
+  }, [tempAsset, disabledSubmit])
 
   const images = asset.images.map(image => {
     return { url: image }
   })
 
-  function editAsset() {
+  function makeRequest(method, requestAction, successAction) {
+    fetch(`/api/assets/${assetId}`, { method: method })
+      .then(response => response.json())
+      .then(responseData => {
+        if (responseData.success) {
+          successAction(router)
+        } else {
+          console.log(`Failed to ${requestAction} asset from server`);
+        }
+      }).catch((error) => {
+        console.error(`Error while trying to ${requestAction}: ${error.message}`);
+      })
+  }
 
+  function editAsset(event) {
+    event.preventDefault()
+    setLoading(true)
+    makeRequest('PUT', 'update', (router) => router.reload())
+    setShowEditModal(false)
+    setLoading(false)
   }
 
   function deleteAsset() {
     setLoading(true)
-    console.log('deleting', router.query.id);
-    fetch(`/api/assets/${router.query.id}`, { method: 'DELETE' })
+    console.log('deleting', assetId);
+    fetch(`/api/assets/${assetId}`, { method: 'DELETE' })
       .then(response => response.json())
       .then(responseData => {
         if (responseData.success) {
@@ -44,8 +67,9 @@ export default function Page() {
       })
   }
 
-  function closeDeleteModal(){
-    setShowDeleteModal(false)
+  function handleChange({ target }) {
+    const { name, value } = target
+    setTempAsset((prevAssetData) => ({ ...prevAssetData, [name]: value }))
   }
 
   return (
@@ -73,18 +97,26 @@ export default function Page() {
           <div className='flex justify-center border-t-2 border-indigo-600 p-2 gap-8'>
             <button
               className="hvr-hang bg-indigo-700 text-white active:bg-indigo-900 font-bold text-sm px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-              type="button">Edit Asset
+              type="button"
+              onClick={() => setShowEditModal(true)}>Edit Asset
             </button>
             <button
               className="hvr-hang bg-red-600 text-white active:bg-red-800 font-bold text-sm px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
               type="button"
               onClick={() => setShowDeleteModal(true)}>Delete Asset
             </button>
+            {showEditModal && <EditAssetForm
+              title={'Edit Asset'}
+              formData={tempAsset}
+              handleChange={handleChange}
+              handleSubmit={editAsset}
+              disabledSubmit={disabledSubmit}
+              onClose={() => setShowEditModal(false)} />}
             {showDeleteModal && !loading && <ConfirmationDialog
               message={'Are you sure you want to delete this asset?'}
               actionButtonText={'Delete'}
-              actionFunction={deleteAsset} 
-              closeFunction={closeDeleteModal}/>
+              actionFunction={deleteAsset}
+              closeFunction={() => setShowDeleteModal(false)} />
             }
             {loading && <Spinner />}
           </div>
@@ -94,7 +126,25 @@ export default function Page() {
   )
 }
 
-Page.getLayout = function getLayout(page) {
+export async function getServerSideProps({ req, res, query }) {
+  const session = await getServerSession(req, res, authOptions)
+
+  if (!session) {
+    return { redirect: { destination: "/auth/signin" } };
+  }
+
+  const assetId = query.id
+  const asset = await findAssetById(assetId)
+
+  return {
+    props: {
+      asset: asset,
+      session: session
+    }
+  }
+}
+
+Asset.getLayout = function getLayout(page) {
   return (
     <DashboardLayout>{page}</DashboardLayout>
   )
